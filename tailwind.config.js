28e4f964294f1a619e2eb57f76bd0031d89ab512
Rakib Hasan <rakibhasan1808@gmail.com>
2024-08-29 12:04:18 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  preline/carousel
+  content: ["./HtmlFile/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+};
+
